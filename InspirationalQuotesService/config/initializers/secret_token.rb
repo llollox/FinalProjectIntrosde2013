89024376e11378549a741e38510c42c12450d5cd
Lorenzo Rigato <lore91tanz@gmail.com>
2014-01-08 11:13:46 +0100

    Add Inspirational Quote Service

---
+++ InspirationalQuotesService/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotes::Application.config.secret_token = '16dcb4bcb9a9d51932631ecbaf528c4368df355370659d37e2a8f93aec70b1ec3a2fe8461e62444d20c78b9307f13a7c81b39bb27e996ff5464f2a97bd7f6f03'
