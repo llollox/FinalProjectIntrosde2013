f8c9c64ceb9e6c024e13894197fede579533cbed
Lorenzo Rigato <lore91tanz@gmail.com>
2014-01-08 10:53:59 +0100

    fix readme

---
+++ README.md
@@ -0,0 +1,9 @@
+##Final Project IntroSDE2013
+<hr/>
+
+###Collaborators:
+*   Endrit Bajo
+*   Francesco Maturi
+*   Lorenzo Rigato
+
+
--- StorageService/Client/src/Main.java
+++ StorageService/Client/src/Main.java
@@ -1,16 +1,11 @@
 import it.unitn.sde.finalproject.*;
 
-import java.util.*;
-
-
-
 public class Main {
 	
 	public static void main(String[] args) {
 		CRUDPerson cperson = new PersonService().getCRUD();
-		CRUDGoal cgoal = new GoalService().getCRUD();
-		
-		cperson.linkgoal(1, 2);
+		Person p = cperson.readPerson(1);
+		System.out.println(p.getFirstname() + " " + p.getLastname());
 	}
 
 }
