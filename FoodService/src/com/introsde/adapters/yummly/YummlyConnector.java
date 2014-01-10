676187825134ff4304db9f63d49f11a06b972c67
francescomaturi <francesco_maturi@tiscali.it>
2014-01-10 18:46:16 +0100

    add one method

--- FoodService/src/com/introsde/adapters/yummly/YummlyConnector.java
+++ FoodService/src/com/introsde/adapters/yummly/YummlyConnector.java
@@ -1,6 +1,9 @@
 package com.introsde.adapters.yummly;
 
+import java.util.List;
+
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
 import com.introsde.adapters.yummly.models.Receipt;
 import com.introsde.adapters.yummly.models.ReceiptFinder;
@@ -8,13 +11,38 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 public class YummlyConnector {
 
-	private static final String YUMMLY_APP_ID = "ad98c4ce";
-	private static final String YUMMLY_APP_KEY = "034eea1cd3f52cdd3a742772fca0d859";
+	private static final String APP_ID_VALUE = "ad98c4ce";
+	private static final String APP_KEY_VALUE = "034eea1cd3f52cdd3a742772fca0d859";
+
+	private static final String APP_ID_KEY = "_app_id";
+	private static final String APP_KEY_KEY = "_app_key";
+
+	private static final String ENERC_KCAL_MAX = "nutrition.ENERC_KCAL.max";
+	private static final String ENERC_KCAL_MIN = "nutrition.ENERC_KCAL.min";
+
+	private static final String MAX_RESULTS = "maxResult";
+
+	private static final String ALLOWED_INGREDIENTS = "allowedIngredient[]";
+	private static final String EXCLUDED_INGREDIENTS = "excludedIngredient[]";
+
+	private static final String TEXT_QUERY = "q";
+
+	private static final String ALLOWED_DIET = "allowedDiet[]";
+
+	private static final String PESCETARIAN = "390^Pescetarian";
+	private static final String LACTO_OVO_VEGETARIAN = "387^Lacto-ovo vegetarian";
+	private static final String LACTO_VEGETARIAN = "388^Lacto vegetarian";
+	private static final String OVO_VEGETARIAN = "389^Ovo vegetarian";
 
 	private static WebResource service;
+
+	enum ATTR_NAME {
+		K, NA, CHOLE, FATRN, FASAT, CHOCDF, FIBTG, PROCNT, VITC, CA, FE, SUGAR, ENERC_KCAL, FAT, VITA_IU
+	}
 
 	static {
 
@@ -30,8 +58,8 @@ public class YummlyConnector {
 			return null;
 
 		return service.path("/recipe").path(recipeId)
-				.queryParam("_app_id", YUMMLY_APP_ID)
-				.queryParam("_app_key", YUMMLY_APP_KEY)
+				.queryParam(APP_ID_KEY, APP_ID_VALUE)
+				.queryParam(APP_KEY_KEY, APP_KEY_VALUE)
 				.accept(MediaType.APPLICATION_JSON).get(Receipt.class);
 	}
 
@@ -44,13 +72,45 @@ public class YummlyConnector {
 		if (maxResults < 1)
 			return null;
 
-		return service.path("/recipes").queryParam("requirePictures", "true")
-				.queryParam("nutrition.ENERC_KCAL.min", minKcal + "")
-				.queryParam("nutrition.ENERC_KCAL.max", maxKcal + "")
-				.queryParam("maxResult", maxResults + "")
-				.queryParam("_app_id", YUMMLY_APP_ID)
-				.queryParam("_app_key", YUMMLY_APP_KEY)
+		return service
+				.path("/recipes")
+				// parameter to get recipes with pictures
+				.queryParam("requirePictures", "true")
+				.queryParam(ENERC_KCAL_MIN, minKcal + "")
+				.queryParam(ENERC_KCAL_MAX, maxKcal + "")
+				.queryParam(MAX_RESULTS, maxResults + "")
+				.queryParam(APP_ID_KEY, APP_ID_VALUE)
+				.queryParam(APP_KEY_KEY, APP_KEY_VALUE)
 				.accept(MediaType.APPLICATION_JSON).get(ReceiptFinder.class);
 	}
 
+	public static void getRecipes(String textQuery,
+			List<String> includedIngredients, List<String> excludedIngredients,
+			int maxResults) {
+
+		service.path("/recipes");
+
+		MultivaluedMap<String, String> map = new MultivaluedMapImpl();
+
+		for (String ingr : includedIngredients) {
+			map.add(ALLOWED_INGREDIENTS, ingr);
+		}
+
+		for (String ingr : excludedIngredients) {
+			map.add(EXCLUDED_INGREDIENTS, ingr);
+		}
+
+		if (!map.isEmpty()) {
+			service.queryParams(map);
+		}
+
+		if (textQuery != null)
+			service.queryParam(TEXT_QUERY, textQuery);
+
+		service.queryParam(APP_ID_KEY, APP_ID_VALUE)
+				.queryParam(MAX_RESULTS, maxResults + "")
+				.queryParam(APP_KEY_KEY, APP_KEY_VALUE)
+				.accept(MediaType.APPLICATION_JSON).get(ReceiptFinder.class);
+
+	}
 }
