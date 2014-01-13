b496f6ff33aff650674e2395a0a5fa527939bd7b
francescomaturi <francesco_maturi@tiscali.it>
2014-01-13 22:38:00 +0100

    food service is fucking awsome and done

--- WebAppRest/src/main/java/introsde/finalproject/service/FoodRestService.java
+++ WebAppRest/src/main/java/introsde/finalproject/service/FoodRestService.java
@@ -1,6 +1,7 @@
 package introsde.finalproject.service;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,22 +11,28 @@ import javax.ws.rs.core.MediaType;
 
 import com.food.client.Food;
 import com.food.client.FoodService;
+import com.introsde.adapters.yummly.Yummly;
 import com.introsde.adapters.yummly.models.Matches;
 import com.introsde.adapters.yummly.models.Recipe;
 import com.introsde.adapters.yummly.models.RecipeFinder;
+import com.introsde.food.utils.KeyValuePair;
 import com.introsde.food.utils.QueryParams;
 
-@Path("/food/recipes")
+@Path("/food")
 public class FoodRestService {
 
 	Food foodService = new FoodService().getFood();
 
 	@GET
+	@Path("/recipes")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public List<Matches> getRecipesByCalories() {
+		List<Matches> list;
+		do {
+			RecipeFinder finder = foodService.getRecipes(getRandomPrams());
+			list = finder.getMatches();
 
-		RecipeFinder finder = foodService.getRecipes(new QueryParams());
-		List<Matches> list = finder.getMatches();
+		} while (list == null || list.size() < 10);
 
 		for (Matches m : list)
 			addImagesUrl(m.getSmallImageUrls());
@@ -34,7 +41,7 @@ public class FoodRestService {
 	}
 
 	@GET
-	@Path("/{recipeId}")
+	@Path("/recipe/{recipeId}")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Recipe getRecipe(@PathParam("recipeId") String recipeId) {
 
@@ -50,4 +57,23 @@ public class FoodRestService {
 		listUrls.add(url.replace(".s.", ".xl."));
 
 	}
+
+	private Random rand = new Random(System.currentTimeMillis());
+
+	private QueryParams getRandomPrams() {
+		QueryParams params = new QueryParams();
+
+		for (int n = 0; n < 2; n++) {
+			params.add(new KeyValuePair(Yummly.ALLOWED_INGREDIENT, ingr[rand
+					.nextInt(ingr.length)]));
+		}
+
+		return params;
+	}
+
+	String[] ingr = new String[] { "onion", "pepper", "butter", "cheese",
+			"salad", "chicken", "chilly", "potato", "fish", "spaghetti",
+			"tuna", "oil", "meat", "tomato", "bacon", "zucchini", "salami",
+			"bread", "egg" };
+
 }
