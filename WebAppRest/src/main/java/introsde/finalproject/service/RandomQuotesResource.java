23656b017b4296f19ce6b09850d90b3fa3a334fe
francescomaturi <francesco_maturi@tiscali.it>
2014-01-15 17:28:24 +0100

    evvaiii

---
+++ WebAppRest/src/main/java/introsde/finalproject/service/RandomQuotesResource.java
@@ -0,0 +1,13 @@
+package introsde.finalproject.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+//@Path("/randomQuote")
+public class RandomQuotesResource {
+
+	
+//	@GET
+//	public RandomQuote
+}
