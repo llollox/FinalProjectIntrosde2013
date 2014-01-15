36712021c2b1f7d8aad13d00a18d3f2c6c523519
francescomaturi <francesco_maturi@tiscali.it>
2014-01-15 16:47:32 +0100

    new files

---
+++ MotivationalQuotesWrapperService/src/introsde/random/server/RandomServer.java
@@ -0,0 +1,16 @@
+package introsde.random.server;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+public class RandomServer {
+
+	public static final String URL_SERVER = "http://localhost:5990/";
+
+	public RandomServer() throws Exception {
+
+		final HttpServer server = HttpServerFactory.create(URL_SERVER);
+		server.start();
+		System.out.println("Random Quotes Service Started");
+	}
+}
